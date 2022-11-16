1eb0afe691ad8184511764924908234e1dbbd239
Brandon Mansfield <Brandon_mansfield@hotmail.es>
2022-11-16 11:54:29 -0300

    Generacion de evidencias Cypress

--- e2e/smoketestbank.cy.js
+++ e2e/smoketestbank.cy.js
@@ -1,10 +1,12 @@
 describe("Test suite - conjunto de pruebas", () => {
 
+    beforeEach(() => {
+        //Acciones a realizar para cada una de las pruebas, reduce repeticion de instrucciones.
+        cy.visit("http://zero.webappsecurity.com")
+    })
 
     it("Validar elementos home page", () => {
 
-        //Indico sitio donde vamos a trabajar
-        cy.visit("http://zero.webappsecurity.com")
         //Indico elemento html y accion a realizar con dicho elemento, ej: validar que este visible la img
         cy.get('.active > img').should("be.visible")
         //valido visibilidad del texto
@@ -13,7 +15,6 @@ describe("Test suite - conjunto de pruebas", () => {
 
     it("Prueba E2E - transferencia bancaria", () => {
 
-        cy.visit("http://zero.webappsecurity.com")
         cy.get('#signin_button').click()
         cy.get('#user_login').type("username")
         cy.get('#user_password').type("password")
@@ -31,7 +32,7 @@ describe("Test suite - conjunto de pruebas", () => {
     })
 
     //Agregar .only para que Cypress corra unicamente la prueba seleccionada
-    it.only("Actualizacion de grafico", () => {
+    it("Visualizar actualizacion de grafico", () => {
 
         cy.visit("http://zero.webappsecurity.com")
         cy.get('#signin_button').click()
@@ -39,7 +40,10 @@ describe("Test suite - conjunto de pruebas", () => {
         cy.get('#user_password').type("password")
         cy.get('.btn').click()
         cy.get('#money_map_tab > a').click()
-        
+        cy.get('#ext-sprite-1259').should("be.visible")
+        cy.get('#ext-sprite-1167 > tspan').click()
+        cy.get('#ext-sprite-1259').should("not.visible")
+
     })
 
 
