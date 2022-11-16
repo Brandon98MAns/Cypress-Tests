9dabdd5b370c070f8b8781f17f2cee9b4774f06c
Brandon Mansfield <Brandon_mansfield@hotmail.es>
2022-11-15 23:35:56 -0300

    E2E Tests

--- e2e/smoketestbank.cy.js
+++ e2e/smoketestbank.cy.js
@@ -1,7 +1,7 @@
 describe("Test suite - conjunto de pruebas", () => {
 
 
-    it("Nombre de la prueba1", () => {
+    it("Validar elementos home page", () => {
 
         //Indico sitio donde vamos a trabajar
         cy.visit("http://zero.webappsecurity.com")
@@ -11,11 +11,34 @@ describe("Test suite - conjunto de pruebas", () => {
         cy.get('.active > .custom > p').contains("Welcome to Zero Online Banking.")
     })
 
-    it("Nombre de la prueba2", () => {
-        
+    it("Prueba E2E - transferencia bancaria", () => {
+
+        cy.visit("http://zero.webappsecurity.com")
+        cy.get('#signin_button').click()
+        cy.get('#user_login').type("username")
+        cy.get('#user_password').type("password")
+        cy.get('.btn').click()
+        cy.get('#transfer_funds_tab > a').click()
+        cy.get('#tf_fromAccountId').select("1")
+        cy.get('#tf_toAccountId').select("5")
+        cy.get('#tf_amount').type("300" + 1)
+        cy.get('#tf_description').type("Transferencia de prueba")
+        cy.get('#btn_submit').click()
+        cy.get('#btn_submit').click()
+        cy.get('.alert').contains("You successfully submitted your transaction.")
+
+
     })
 
-    it("Nombre de la prueba3", () => {
+    //Agregar .only para que Cypress corra unicamente la prueba seleccionada
+    it.only("Actualizacion de grafico", () => {
+
+        cy.visit("http://zero.webappsecurity.com")
+        cy.get('#signin_button').click()
+        cy.get('#user_login').type("username")
+        cy.get('#user_password').type("password")
+        cy.get('.btn').click()
+        cy.get('#money_map_tab > a').click()
         
     })
 
